10c0ff6a1973eec799f71d64a66acbe79db7ee97
Owen Dods <owen.dods@gmail.com>
2016-02-27 13:08:01 +0000

    Move HeroesGrid into its own component

---
+++ src/components/HeroesGrid.js
@@ -0,0 +1,38 @@
+var React = require('react');
+
+module.exports = React.createClass({
+
+	render: function () {
+
+		var heroHolders = this.props.data ? this.props.data.result.heroes.map(function(hero) {
+
+			hero.formattedName = hero.name.replace('npc_dota_hero_', '');
+
+			return (
+				<li className="heroesGrid__hero" key={hero.id}>
+
+					<div className="heroesGrid__avatar">
+
+						<img src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + hero.formattedName + '_hphover.png?v=3162717'} />
+
+					</div>
+
+					<label>{hero.localized_name}</label>
+
+				</li>
+			);
+		}) : null;
+
+		return (
+
+			<ul>
+
+				{heroHolders}
+
+			</ul>
+
+		);
+
+	}
+
+});
--- src/index.js
+++ src/index.js
@@ -173,42 +173,7 @@ var CommentForm = React.createClass({
 // 	document.getElementById('content')
 // );
 
-var HeroesGrid = React.createClass({
-
-	render: function () {
-
-		var heroHolders = this.props.data ? this.props.data.result.heroes.map(function(hero) {
-
-			hero.formattedName = hero.name.replace('npc_dota_hero_', '');
-
-			return (
-				<li className="heroesGrid__hero" key={hero.id}>
-
-					<div className="heroesGrid__avatar">
-
-						<img src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + hero.formattedName + '_hphover.png?v=3162717'} />
-
-					</div>
-
-					<label>{hero.localized_name}</label>
-
-				</li>
-			);
-		}) : null;
-
-		return (
-
-			<ul>
-
-				{heroHolders}
-
-			</ul>
-
-		);
-
-	}
-
-});
+var HeroesGrid = require('./components/HeroesGrid');
 
 ReactDOM.render(
 	<HeroesGrid data={heroData} />,
