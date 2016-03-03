5ea88b12135e6a548cca40f6881ef87cb1768c39
Owen Dods <owen.dods@gmail.com>
2016-03-03 15:53:50 +0000

    Tweak heroAvatar styles to use background-image Implement standard box-shadow variables and mixing

--- src/components/HeroAvatar.js
+++ src/components/HeroAvatar.js
@@ -18,11 +18,7 @@ module.exports = React.createClass({
 
 		return (
 
-			<div className="heroAvatar">
-
-				<img src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + this.state.heroName + '_hphover.png?v=3162717'} />
-
-			</div>
+			<div className="heroAvatar" style={{backgroundImage: 'url(http://cdn.dota2.com/apps/dota2/images/heroes/' + this.state.heroName + '_hphover.png?v=3162717)'}}></div>
 
 		);
 
